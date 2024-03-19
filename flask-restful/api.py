730231a6f63100b65634a68573fb5a823873db6f
adammazur016 <adam.mazur016@gmail.com>
2024-03-19 20:29:36 +0100

    testing

--- flask-restful/api.py
+++ flask-restful/api.py
@@ -6,34 +6,13 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 api = Api(app)
 
-app.config['MYSQL_HOST'] = '172.18.0.2'
-app.config['MYSQL_USER'] = 'db_user'
-app.config['MYSQL_PASSWORD'] = 'db_user_pass'
-app.config['MYSQL_DB'] = 'app_db'
-
-mysql = MySQL(app)
-
-
-def testDatabase():
-    # Creating a connection cursor
-    cursor = mysql.connection.cursor()
-
-    # Executing SQL Statements
-    cursor.execute(''' INSERT INTO users VALUES(null, 'test', 'test', 'test') ''')
-
-    # Saving the Actions performed on the DB
-    mysql.connection.commit()
-
-    # Closing the cursor
-    cursor.close()
-
 
 class login(Resource):
-    #method_decorators = [auth]
+    method_decorators = [auth]
     def get(self):
         # code 1 - login successfully
         # code 2 - login failed
-        testDatabase()
+        #testDatabase()
         return {'hello': 'yellow man'}
 
 
--- flask-restful/auth_methods.py
+++ flask-restful/auth_methods.py
@@ -1,8 +1,37 @@
+from flask import Flask
 from functools import wraps
 from flask import request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = '172.18.0.2'
+app.config['MYSQL_USER'] = 'db_user'
+app.config['MYSQL_PASSWORD'] = 'db_user_pass'
+app.config['MYSQL_DB'] = 'app_db'
+
+mysql = MySQL(app)
+
+
+def testDatabase():
+    # Creating a connection cursor
+    cursor = mysql.connection.cursor()
+
+    # Executing SQL Statements
+    cursor.execute(''' INSERT INTO users VALUES(null, 'test', 'test', 'test') ''')
+
+    # Saving the Actions performed on the DB
+    mysql.connection.commit()
+
+    # Closing the cursor
+    cursor.close()
+
 
 def access_denied():
-    return {'error_message': 'Invalid API key'}, 401
+    return {'error_message': 'Not correct credentials'}, 401
+
+
+def access_granted():
+    return {'response_code': '1'}
 
 
 def admin_verify(admin_api_key: str):
@@ -13,9 +42,11 @@ def admin_verify(admin_api_key: str):
         return False
 
 
-def verify(api_key: str):
+def verify(login: str):
     # To-Do: Implement verification via db
-    if api_key == 'test':
+
+    if login == 'test':
+        testDatabase()
         return True
     else:
         return False
@@ -34,8 +65,8 @@ def admin_auth(func):
 def auth(func):
     @wraps(func)
     def verify_key(*args, **kwargs):
-        if request.args.get('api_key') and verify(request.args.get('api_key')):
-            return func(*args, **kwargs)
+        if request.args.get('login') and verify(request.args.get('login')):
+            return access_granted()
         else:
             return access_denied()
     return verify_key
