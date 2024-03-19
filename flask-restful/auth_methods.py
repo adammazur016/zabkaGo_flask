3e6efb6e293c2ab2667621245948a5db2ce1f9c9
Krzysztof Perdek <krzysztofperdek@gmail.com>
2024-03-19 17:49:54 +0100

    Implement authentication decorator

---
+++ flask-restful/auth_methods.py
@@ -0,0 +1,41 @@
+from functools import wraps
+from flask import request
+
+def access_denied():
+    return {'error_message': 'Invalid API key'}, 401
+
+
+def admin_verify(admin_api_key: str):
+    # To-Do: Implement verification via db
+    if admin_api_key == 'test':
+        return True
+    else:
+        return False
+
+
+def verify(api_key: str):
+    # To-Do: Implement verification via db
+    if api_key == 'test':
+        return True
+    else:
+        return False
+
+
+def admin_auth(func):
+    @wraps(func)
+    def verify_key(*args, **kwargs):
+        if request.args.get('api_key') and admin_verify(request.args.get('api_key')):
+            return func(*args, **kwargs)
+        else:
+            return access_denied()
+    return verify_key
+
+
+def auth(func):
+    @wraps(func)
+    def verify_key(*args, **kwargs):
+        if request.args.get('api_key') and verify(request.args.get('api_key')):
+            return func(*args, **kwargs)
+        else:
+            return access_denied()
+    return verify_key
