e46b934be7545ee89a47ed54c961af32aedfc00b
adammazur016 <adam.mazur016@gmail.com>
2024-03-24 23:57:54 +0100

    database integration

--- flask-restful/api.py
+++ flask-restful/api.py
@@ -1,19 +1,62 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from auth_methods import admin_auth, auth
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 api = Api(app)
 
+app.config['MYSQL_HOST'] = '172.18.0.2'
+app.config['MYSQL_USER'] = 'db_user'
+app.config['MYSQL_PASSWORD'] = 'db_user_pass'
+app.config['MYSQL_DB'] = 'app_db'
 
-class HelloWorld(Resource):
-    method_decorators = [auth]
-
-    def get(self):
-        return {'hello': 'world'}
+mysql = MySQL(app)
 
 
-api.add_resource(HelloWorld, '/')
+def testDatabase(loginGiven, passwordGiven):
+
+    myLogin = "'" + loginGiven + "'"
+    myPass = "'" + passwordGiven + "'"
+    # Creating a connection cursor
+    cursor = mysql.connection.cursor()
+
+    # Executing SQL Statements
+
+    query = "SELECT password FROM users WHERE login = " + myLogin
+
+    print(f'The query is: {query}')
+
+    cursor.execute(query)
+
+    data = cursor.fetchall()
+
+    correctPassword = data[0][0]
+
+    print(f'The password for user {myLogin} is {correctPassword}')
+
+    # Saving the Actions performed on the DB
+   # mysql.connection.commit()
+
+    # Closing the cursor
+    cursor.close()
+
+    return correctPassword
+
+
+class login(Resource):
+    #method_decorators = [auth]
+    def get(self, login3, password):
+        correctPwd = testDatabase(login3, password)
+        # code 1 - login2 successfully
+        # code 2 - login2 failed
+        if correctPwd == password:
+            return {'auth': '1'}
+        else:
+            return {'auth': '-1'}
+
+
+api.add_resource(login, '/login/<string:login3>/<string:password>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=50000)
--- flask-restful/auth_methods.py
+++ flask-restful/auth_methods.py
@@ -1,8 +1,9 @@
 from functools import wraps
 from flask import request
 
+
 def access_denied():
-    return {'error_message': 'Invalid API key'}, 401
+    return {'error_message': 'Not correct credentials'}, 401
 
 
 def admin_verify(admin_api_key: str):
@@ -13,9 +14,9 @@ def admin_verify(admin_api_key: str):
         return False
 
 
-def verify(api_key: str):
+def verify(login: str):
     # To-Do: Implement verification via db
-    if api_key == 'test':
+    if login == 'test':
         return True
     else:
         return False
