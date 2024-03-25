2fa6caa0616112b8073b064a6c74229cb0ba88cd
Krzysztof Perdek <krzysztofperdek@gmail.com>
2024-03-25 14:28:38 +0100

    Move HOST and PORT to .flaskenv

--- flask-restful/src/config/config.py
+++ flask-restful/src/config/config.py
@@ -13,10 +13,6 @@ class Config:
         # Flask Part
         if os.environ.get("APP_NAME"):
             self.config.APP_NAME = os.environ.get("APP_NAME")
-        if os.environ.get("PORT"):
-            self.config.PORT = os.environ.get("PORT")
-        if os.environ.get("HOST"):
-            self.config.HOST = os.environ.get("HOST")
         # MySQL part
         if os.environ.get("MYSQL_HOST"):
             self.config.MYSQL_HOST = os.environ.get("MYSQL_HOST")
--- flask-restful/src/config/default_config.py
+++ flask-restful/src/config/default_config.py
@@ -2,8 +2,6 @@ class DefaultConfig:
     def __init__(self):
         # App
         self.APP_NAME = "Zabka Go"
-        self.PORT = 50000
-        self.HOST = '0.0.0.0'
         # MySQL
         self.MYSQL_HOST = '0.0.0.0'
         self.MYSQL_USER = 'user'
