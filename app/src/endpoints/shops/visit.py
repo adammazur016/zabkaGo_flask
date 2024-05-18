bbbcc884fc1fb5afe32667578b684c47c45fc442
Krzysztof Perdek <krzysztofperdek@gmail.com>
2024-05-18 20:00:16 +0200

    Refactor visit.py

--- app/src/endpoints/shops/visit.py
+++ app/src/endpoints/shops/visit.py
@@ -8,49 +8,40 @@ from src import achievements
 
 visit_endpoint = Blueprint('visit', __name__)
 
-
-def mark_visit_query(session_token, shop_id) -> dict:
+def mark_visit_query(user_id, shop_id):
     """
     Updates the date of the last visit in the shop with the provided ID.
     Identifies the user based on the session token.
 
-    :param session_token: The session token of the user.
+    :param user_id: The ID of the user.
     :param shop_id: The ID of the shop.
-    :return: The response ready for JSON serialization.
     """
     with mysql.connector.connect(**app_config.MYSQL_CONFIG) as cnx:
         with cnx.cursor() as cursor:
             today_date = date.today().strftime('%Y-%m-%d')
-            user_id = get_user_id(session_token)
             query = f"INSERT INTO visits VALUES ({user_id}, {shop_id}, '{today_date}')"
             cursor.execute(query)
         cnx.commit()
-    return {"status": "success", "message": "visit_done"}
 
 
-def check_visit_query(session_token, shop_id) -> dict:
+def check_visit_query(user_id, shop_id) -> dict:
     """
     Checks if the user is allowed to visit the shop with the provided ID.
     Identifies the user based on the session token.
 
-    :param session_token: The session token of the user.
+    :param user_id: The ID of the user.
     :param shop_id: The ID of the shop to be visited.
     :return: The response ready for JSON serialization.
     """
     with mysql.connector.connect(**app_config.MYSQL_CONFIG) as cnx:
         with cnx.cursor() as cursor:
-            user_id = get_user_id(session_token)
             query = f"SELECT date FROM visits WHERE user_id = {user_id} AND place_id = {shop_id} ORDER BY date DESC LIMIT 1;"
             cursor.execute(query)
             query_result = cursor.fetchall()
             # There is no information that the user ever visited the shop, it will be his first visit
             if len(query_result) == 0:
-                return {"status": "success", "message": "visit_possible"}
-
-    if date.today() == query_result[0][0]:
-        return {"status": "fail", "message": "visit_impossible"}
-
-    return {"status": "success", "message": "visit_possible"}
+                return "never"
+    return query_result[0][0]
 
 
 def get_user_visits(user_id) -> list[dict]:
@@ -87,19 +78,21 @@ def make_visit(shop_id) -> (Response, int):
     :return: JSON-serialized response, along with the corresponding HTTP status code.
     """
     session_token = request.args.get("session_token")
+    user_id = get_user_id(session_token)
     # Check if shop exists
     if not does_shop_exist(shop_id):
         return jsonify({"status": "fail", "message": "shop_not_found"}), 404
     # Check if visit is allowed
-    check_query_result = check_visit_query(session_token, shop_id)
-    if check_query_result["status"] == "fail":
-        return jsonify(check_query_result), 403
-    # Mark visit
-    mark_query_result = mark_visit_query(session_token, shop_id)
-    # Add ranked points
-    add_rank_point(session_token)
-    # Send reply
-    return jsonify(mark_query_result), 200
+    if check_visit_query(user_id, shop_id) != date.today():
+        # Mark visit
+        mark_visit_query(user_id, shop_id)
+        # Add ranked points
+        add_rank_point(user_id)
+        # Send reply
+        return jsonify({"status": "success", "message": "visit_done"}), 200
+    else:
+        return jsonify({"status": "fail", "message": "visit_impossible"}), 403
+
 
 
 @visit_endpoint.route('/shop/<shop_id>/visit', methods=['GET'])
@@ -120,13 +113,12 @@ def check_visit(shop_id) -> (Response, int):
         return jsonify({"status": "fail", "message": "shop_not_found"}), 404
 
     session_token = request.args.get("session_token")
-    query_result = check_visit_query(session_token, shop_id)
+    user_id = get_user_id(session_token)
 
-    # Status code 200 if visit is allowed, 403 if forbidden
-    if query_result["status"] == "success":
-        return jsonify(query_result), 200
+    if check_visit_query(user_id, shop_id) != date.today():
+        return jsonify({"status": "success", "message": "visit_possible"}), 200
     else:
-        return jsonify(query_result), 403
+        return jsonify({"status": "fail", "message": "visit_impossible"}), 403
 
 
 @visit_endpoint.route('/user/<user_id>/visits', methods=['GET'])
